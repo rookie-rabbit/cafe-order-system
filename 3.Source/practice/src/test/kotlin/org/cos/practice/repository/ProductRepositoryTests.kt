d0b48de7c28dc597540eb37ad764f531c9e03937
조경현 <jkh9615@naver.com>
2022-01-17 12:15:04 +0900

    Test 코드 추가 (Read / Delete / Update)

--- 3.Source/practice/src/test/kotlin/org/cos/practice/repository/ProductRepositoryTests.kt
+++ 3.Source/practice/src/test/kotlin/org/cos/practice/repository/ProductRepositoryTests.kt
@@ -1,9 +1,13 @@
 package org.cos.practice.repository
 
+import org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.isEquals
 import org.cos.practice.Entity.ProductEntity
+import org.cos.practice.dto.ProductDTO
+import org.cos.practice.service.ProductService
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.util.Assert
 import java.time.LocalDateTime
 import javax.transaction.Transactional
 
@@ -15,11 +19,35 @@ class ProductRepositoryTests (){
     @Autowired
     private lateinit var productRepository: ProductRepository
 
+    @Autowired
+    private lateinit var service: ProductService
+
     @Test
     @Transactional
     public fun insertDummies() {
         val product = ProductEntity(pID = 1, pName = "a", pPrice = 1, pCntPerDay = 1, pLastSellDate = LocalDateTime.now(), pIsShow = true)
 
         productRepository.save(product)
+    }
+
+    @Test
+    fun readTest(){
+        val pID = 10L
+        val product = ProductDTO(pID=10, pName = "test", pPrice = 10, pCntPerDay = -1, pLastSellDate = LocalDateTime.now(), pIsShow = false)
+        val productObj: ProductDTO? = service.read(pID)
+
+        if(productObj == null)
+         Assert.isTrue(false)
+        else{
+            Assert.isTrue(productObj.pID == 10L)
+        }
+    }
+
+    @Test
+    fun deleteTest(){
+        val pID = 100L
+        val ret:Long = service.delete(pID)
+
+        Assert.isTrue(pID == ret)
     }
 }
