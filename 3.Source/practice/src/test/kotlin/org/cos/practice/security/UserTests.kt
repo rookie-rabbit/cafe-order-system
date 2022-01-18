98136acaa5024a71eeba7020784c7d043f3d3d4e
조재국 <hoxymola@gmail.com>
2022-01-18 16:44:04 +0900

    branch test

--- 3.Source/practice/src/test/kotlin/org/cos/practice/security/UserTests.kt
+++ 3.Source/practice/src/test/kotlin/org/cos/practice/security/UserTests.kt
@@ -19,27 +19,27 @@ class UserTests {
     @Autowired
     private lateinit var service: UserService
 
-    @Test
-    fun insertDummies() {
-        for (i in 1..9) {
-            val user = UserEntity(uID = "user$i@nhn-commerce.com", uPW = passwordEncoder.encode("1111"), uRL = 1)
-            userRepository.save(user)
-        }
-        val admin = UserEntity(uID = "admin@nhn-commerce.com", uPW = passwordEncoder.encode("1111"), uRL = 2)
-        userRepository.save(admin)
-    }
-
 //    @Test
-//    fun deleteTest() {
+//    fun insertDummies() {
 //        for (i in 1..9) {
-//            val uID: String = "user$i@nhn-commerce.com"
-//            val ret: String = service.delete(uID)
-//
-//            Assert.isTrue(uID.equals(ret))
+//            val user = UserEntity(uID = "user$i@nhn-commerce.com", uPW = passwordEncoder.encode("1111"), uRL = 1)
+//            userRepository.save(user)
 //        }
-//        val uID: String = "admin@nhn-commerce.com"
-//        val ret: String = service.delete(uID)
-//
-//        Assert.isTrue(uID.equals(ret))
+//        val admin = UserEntity(uID = "admin@nhn-commerce.com", uPW = passwordEncoder.encode("1111"), uRL = 2)
+//        userRepository.save(admin)
 //    }
+
+    @Test
+    fun deleteTest() {
+        for (i in 1..9) {
+            val uID: String = "user$i@nhn-commerce.com"
+            val ret: String = service.delete(uID)
+
+            Assert.isTrue(uID.equals(ret))
+        }
+        val uID: String = "admin@nhn-commerce.com"
+        val ret: String = service.delete(uID)
+
+        Assert.isTrue(uID.equals(ret))
+    }
 }
