18b16a21bcc50e34e28b04543e04fefc7cc6742a
gudwnsgur <gudwnsgur12@naver.com>
2022-01-18 14:47:45 +0900

    조재국 - UserEntity

---
+++ 3.Source/practice/src/main/kotlin/org/cos/practice/entity/UserEntity.kt
@@ -0,0 +1,22 @@
+package org.cos.practice.entity
+
+import lombok.*
+import javax.persistence.*
+
+@Entity
+@Getter
+@Setter
+@Builder
+@Table(name = "COUser")
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+data class UserEntity(
+    @Id @Column(name = "uID", length = 100) val uID: String = "",
+    @Column(name = "uPW", length = 100) var uPW: String = "",
+    @Column(name = "uRL") val uRL: Int = 1
+) {
+    fun changePassword(name: String) {
+        this.uPW = name
+    }
+}
